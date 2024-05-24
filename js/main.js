f551d97dbe815342c99f21d70003131f741e5017
Trach Volodymyr <vtlviv90@gmail.com>
2024-05-24 15:23:30 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+const swiper = new Swiper('.reviews__slider', {
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      grid: {
+        rows: 2,
+        fill: 'row'
+      },
+      navigation: {
+        nextEl: '.reviews__item-prev',
+        prevEl: '.reviews__item-next',
+      },
+      loop: true,
+      spaceBetween: 22,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+const swiperTeam = new Swiper('.team__inner', {
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+      navigation: {
+        nextEl: '.reviews__item-prev',
+        prevEl: '.reviews__item-next',
+      },
+      loop: true,
+      spaceBetween: 40,
+    },
+    361: {
+      slidesPerView: 0,
+    }
+  },
+});
+
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  menuList.classList.toggle('menu--open');
+});
+
+
+let accordeonTitle = document.querySelectorAll('.accordeon__title');
+
+accordeonTitle.forEach(item => {
+  item.addEventListener('click', () => {
+
+    const parent = item.parentNode;
+
+    if (parent.classList.contains('accordeon__item--active')) {
+      parent.classList.remove('accordeon__item--active')
+    } else {
+      accordeonTitle.forEach(element => {
+        element.parentNode.classList.remove('accordeon__item--active')
+      })
+      parent.classList.add('accordeon__item--active')
+    }
+
+  })
+});
